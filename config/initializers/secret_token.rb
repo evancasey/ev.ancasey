79a3349aaf414329f9497ce32b85b41fe7bbfb60
evancasey <evan.casey92@gmail.com>
2013-06-02 17:16:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvAncasey::Application.config.secret_token = '3456a370be8f0d25f6c6c2f38def3d3947c2ff8c5eb6868276fb8608c85b85105c3e271a0b394e17c53928d49e96e35387a710c073b4f837c5768b25d71c4966'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EvAncasey::Application.config.session_store :cookie_store, key: '_ev.ancasey_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EvAncasey::Application.config.session_store :active_record_store
