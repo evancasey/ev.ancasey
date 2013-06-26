4fee626b2b638a42689458bbc6673951fbff4ebc
Evan Casey <Evan@Evans-MacBook-Pro-2.local>
2013-06-25 22:32:15 -0700

    fixed up homepage css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,29 @@
+require 'redcarpet'
 module ApplicationHelper
-  def markdown(text)
-    extensions = {autolink: true, space_after_headers: true,
-                  hard_wrap: true, filter_html: true,
-                  no_intraemphasis: true, fenced_code: true,
-                  gh_blockcode: true}
-
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
-
-    markdown.render(text).html_safe
-
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true)
+    @markdown.render(content)
   end
-
 end
+#require 'redcarpet'
+#
+#module ApplicationHelper
+#  #def markdown(text)
+#    #extensions = {autolink: true, space_after_headers: true,
+#    #              hard_wrap: true, filter_html: true,
+#    #              no_intraemphasis: true, fenced_code: true,
+#    #              gh_blockcode: true}
+#    #
+#    #markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
+#    #
+#    #markdown.render(text).html_safe
+#
+#  def markdown(text)
+#    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+#                                       :autolink => true, :space_after_headers => true)
+#    return markdown.render(text)
+#  end
+#
+#  #end
+#
+#end
