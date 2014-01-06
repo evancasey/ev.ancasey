42e29bf4f340d16c906b3da82c9b097c312d9539
Evan Casey <evan.casey92@gmail.com>
2014-01-06 11:40:53 -0700

    assets

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,22 +2,3 @@
 // All this logic will automatically be available in application.js.
 
 
-$(function(){
-
-	$('.main .grid img').mouseenter(function(e) {
-	  e.preventDefault();        
-	  $(this).css('border-color','#AAA');
-	  $(this).closest('.thumb').children('.desc').css('color','#888');
-	  $(this).closest('.thumb').children('.desc').css('background-color','yellow');
-	});
-	$('.main .grid img').mouseleave(function(e) {
-	  e.preventDefault();
-	  $(this).css('border-color','#EEE');	 
-	  $(this).closest('.thumb').children('.desc').css('color','#ae2900'); 
-	  $(this).closest('.thumb').children('.desc').css('background-color','white');
-	});
-
-});
-
-// twitter button
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
